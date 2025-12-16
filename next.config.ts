394703186918e153794f7a21dd58bc0e382b4853
SESHAMSURYAVAMSI <suryavamsis005@gmail.com>
2025-12-16 11:43:03 +0530

    fixed

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'img.youtube.com',
-        port: '',
-        pathname: '/vi/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.vimeocdn.com',
-        port: '',
-        pathname: '/video/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
